7767ba17402e26473aa7cb7c891d1f75c842327d
Dominik <szeplakidominik888@gmail.com>
2025-04-17 16:25:03 +0200

    form letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -1,7 +1,7 @@
 /**
  * A `Terulet` osztály egy HTML elemet hoz létre és hozzáadja azt egy konténerhez.
  */
-class Terulet { 
+class Terulet {
     /**
      * @type {HTMLDivElement}
      */
@@ -10,7 +10,7 @@ class Terulet {
     /**
      * @returns {HTMLDivElement} - A létrehozott div elem
      */
-    get div(){ // Getter metódus, amely visszaadja a #div mező értékét}
+    get div() { // Getter metódus, amely visszaadja a #div mező értékét}
         return this.#div; // Visszaadja a privát #div mező értékét
     }
 
@@ -48,12 +48,59 @@ class Tablazat extends Terulet {
         const tablaFejSor = document.createElement('tr'); // Létrehoz egy új 'tr' elemet, ami a táblázat fejlécének sorát képviseli
         tablaFejlec.appendChild(tablaFejSor); // Hozzáadja a 'tr' elemet a fejléchez
         const tablaFejCella = ['név', 'szolgálat', 'évszám', 'felfedezés']; // Létrehozunk egy tömböt, ami a fejléc celláinak tartalmát tárolja
-        for(const cellaTartalom of tablaFejCella){ // Végigiterálunk a fejléc celláinak tartalmán
+        for (const cellaTartalom of tablaFejCella) { // Végigiterálunk a fejléc celláinak tartalmán
             const thCella = document.createElement('th'); // Létrehozunk egy új 'th' elemet, ami a táblázat fejlécének cellája lesz
             thCella.innerText = cellaTartalom; // Beállítjuk a cella szövegét a cellaTartalom változó értékére
             tablaFejSor.appendChild(thCella); // Hozzáadjuk a cellát a fejléc sorhoz
         }
         const tablaTest = document.createElement('tbody'); // Létrehozunk egy új 'tbody' elemet, ami a táblázat törzsét képviseli
         tabla.appendChild(tablaTest); // Hozzáadjuk a 'tbody' elemet a táblázathoz
+    }
+}
+
+/**
+ * A `Urlap` osztály egy űrlapot hoz létre és hozzáadja a konténerhez.
+ * @extends Terulet
+ */
+class Urlap extends Terulet {
+    /**
+     * @type {HTMLFormElement}
+     */
+    constructor(cssOsztaly) { 
+        super(cssOsztaly); // Meghívja a szülő osztály konstruktorát, hogy létrehozza a div elemet
+        const urlap = document.createElement('form'); // Létrehoz egy új 'form' elemet
+        this.div.appendChild(urlap); // Hozzáadja az űrlapot a div-hez
+        const mezoLista = [{ // Létrehozunk egy tömböt, ami a mezőket tárolja
+            mezoid: 'nev', // A mező azonosítója 
+            mezocimke: 'név' // A mező címkéje
+        },
+        {
+            mezoid: 'szolgalat', // A mező azonosítója
+            mezocimke: 'szolgálat' // A mező címkéje
+        },
+        {
+            mezoid: 'evszam', // A mező azonosítója
+            mezocimke: 'évszám' // A mező címkéje
+        },
+        {
+            mezoid: 'felfedezes', // A mező azonosítója
+            mezocimke: 'felfedezés' // A mező címkéje
+        }]
+
+        for (const mezoElem of mezoLista) { // Végigiterálunk a mezoElemLista tömb elemein
+            const mezo = csinalDiv('field'); // Meghívjuk a csinalDiv függvényt 'field' osztálynévvel, és eltároljuk az eredményt mezo néven
+            urlap.appendChild(mezo); // Hozzáadjuk a mezo-t az űrlaphoz
+            const cimke = document.createElement('label'); // Létrehozunk egy új 'label' elemet
+            cimke.htmlFor = mezoElem.mezoid; // Beállítjuk a címke htmlFor attribútumát a mező azonosítójára
+            cimke.textContent = mezoElem.mezocimke; // Beállítjuk a címke szövegét a mező címkéjére
+            mezo.appendChild(cimke) // Hozzáadjuk a címkét a mezőhöz
+            const input = document.createElement('input'); // Létrehozunk egy új 'input' elemet
+            input.id = mezoElem.mezocimke; // Beállítjuk az input azonosítóját a mező azonosítójára
+            mezo.appendChild(document.createElement('br')) // Hozzáadunk egy új 'br' elemet a mezőhöz, hogy új sort hozzunk létre
+            mezo.appendChild(input) // Hozzáadjuk az input elemet a mezőhöz
+        }
+        const gomb = document.createElement('button'); // Létrehozunk egy új 'button' elemet
+        gomb.textContent = 'hozzáadás'; // Beállítjuk a gomb szövegét 'hozzáadás'-ra
+        urlap.appendChild(gomb); // Hozzáadjuk a gombot az űrlaphoz
     }
 }
--- oop/main2.js
+++ oop/main2.js
@@ -1,4 +1,4 @@
 const elvalaszto = document.createElement('hr'); // hogy a html-en egyszeruen megtalalhato legyen az elvalaszto oop es sima kozott
 document.body.appendChild(elvalaszto); // Hozzáadunk egy elválasztó elemet a body-hoz, hogy vizuálisan elválasszuk az OOP és a sima kódot
 const tabla = new Tablazat('table'); // A Terulet osztály példányosítása, létrehozva egy új 'table' osztályú elemet
-const urlap = new Terulet('form'); // A Terulet osztály példányosítása, létrehozva egy új 'table' és 'form' osztályú elemet
+const urlap = new Urlap('form'); // A Terulet osztály példányosítása, létrehozva egy új 'table' és 'form' osztályú elemet
